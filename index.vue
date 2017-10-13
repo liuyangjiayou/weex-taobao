1d6d8912270eabf61bd7b049f892c9a77c38ed53
liuyang <451629802@qq.com>
2017-10-13 18:04:15 +0800

    home

---
+++ index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div class="nav">
+    	<div class="long">
+    		<text class="longText">淘宝优惠卷</text>
+    	</div>
+    	<div class="sousuo">
+    		<input type="" name="" id="" value="" placeholder="好宝贝 等你搜"/>
+    		<image></image>
+    	</div>
+    	<div class="classfy">
+    		<image></image>
+    		<text>分类</text>
+    	</div>
+    	<div class="infoProt">
+    		<image></image>
+    		<text>客户端</text>
+    	</div>
+    </div>
+  </div>
+</template>
+
+<style>
+  .nav{
+  	width : 750px;
+  	height: 88px;
+  	line-height: 88px;
+  	background-color: #fe4a65;
+  	flex-direction: row;
+  	padding-left: 10px;
+  	padding-top: 8px;
+  	padding-right: 10px;
+  	padding-bottom: 8px;
+  	align-items: center;
+  }
+  .long{
+  	width: 192px;
+  	height: 56px;
+  	font-weight: 900;
+  }
+  .longText{
+  	font-size: 38px;
+  	color: #fff;
+  }
+  .sousuo{
+  	width: 316px;
+  	height: 56px;
+  }
+</style>
+
+<script>
+  export default {
+    data: {
+      
+    },
+    methods: {
+      
+    }
+  }
+</script>
